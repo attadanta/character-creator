d0d78752efa796bcfc0bc50015e0d45c6d3d2218
Kiril Tonev <kiril.tonev@gmail.com>
2021-12-09 19:50:43 +0100

    A solution for the ground plane.

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,12 +25,13 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.ground}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
             Customize your character's look and style using the controls below. What sort of adventure will you embark
-            on?{" "}
+            on?
           </p>
         </header>
         <div className={styles.controlColumn}>
